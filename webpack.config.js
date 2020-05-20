4fe099f8906c3aed89792240885c180f3fea12d1
Philip Hurst <youngphil9@gmail.com>
2020-05-20 07:04:01 -0500

    installed dev dependencies babel and webpack with some addons

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+
+const config = {
+  mode: "production",
+  entry: "./public/index.js",
+  output: {
+    path: __dirname + "/public/dist",
+    filename: "bundle.js"
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: false,
+      name: "Budget App",
+      short_name: "Budget App",
+      description: "An application for managing a budget both online and offline",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      start_url: "/",
+    }),
+    
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
+};
+module.exports = config;
